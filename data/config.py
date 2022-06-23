2517dcfcf60857ad492ab5de302b3fd54cad06aa
otabeknurmatov1 <otabekaziastyle@gmail.com>
2022-06-23 18:35:55 +0500

    Deploy heroku

--- data/config.py
+++ data/config.py
@@ -5,14 +5,15 @@
 # env.read_env()
 #
 # # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
-# PROVIDE_TOKEN = env.str("PROVIDER_TOKEN")
+# PROVIDER_TOKEN = env.str("PROVIDER_TOKEN")
 
 import os
 
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = list(os.environ.get("ADMINS"))
-IP = str(os.environ.get("ip"))
-PROVIDEr_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environ.get("ip"))  # Xosting ip manzili
+PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
